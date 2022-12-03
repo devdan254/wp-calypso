8add26a420802752fa639eb56608aee2c53d5669
Edwin Takahashi <egao@outlook.com>
2022-12-02 16:45:56 -0800

    Add debugging to handleEvent

--- packages/calypso-e2e/src/jest-playwright-config/environment.ts
+++ packages/calypso-e2e/src/jest-playwright-config/environment.ts
@@ -272,10 +272,11 @@ class JestEnvironmentPlaywright extends NodeEnvironment {
 						this.testArtifactsPath,
 						`${ artifactFilename }__${ contextIndex }-${ pageIndex }`
 					);
+					console.log( `Browser is connected: ${ this.global.browser.isConnected() }` );
 					try {
 						await page.screenshot( { path: `${ mediaFilePath }.png`, timeout: env.TIMEOUT } );
 					} catch {
-						console.error( `Failed to capture screenshot for test: ${ this.testFilename }}.` );
+						console.error( `Failed to capture screenshot for test: ${ this.testFilename }.` );
 					}
 					await page.close(); // Needed before saving video.
 					try {
@@ -300,9 +301,7 @@ class JestEnvironmentPlaywright extends NodeEnvironment {
 	 * Handle events emitted by jest-circus.
 	 */
 	async handleTestEvent( event: Circus.Event, state: Circus.State ) {
-		if ( this.testFilename.includes( 'infrastructure__ssr' ) ) {
-			console.log( `Event: ${ event.name }` );
-		}
+		console.log( `Event for ${ this.testFilename }: ${ event.name }` );
 		switch ( event.name ) {
 			case 'run_start':
 				this.allure?.startTestFile( this.testFilename );
@@ -347,6 +346,9 @@ class JestEnvironmentPlaywright extends NodeEnvironment {
 					event.test.mode = 'skip';
 				}
 			case 'test_fn_start':
+				console.log(
+					`In test_fn_start, is browser connected: ${ this.global.browser.isConnected() }`
+				);
 				// This event is fired after both the `beforeAll` and `beforeEach` hooks.
 				// Since this event fires after `beforeEach` hooks, it is the best way to detect
 				// an actual `it/test` step as having started.
@@ -366,6 +368,9 @@ class JestEnvironmentPlaywright extends NodeEnvironment {
 				break;
 			}
 			case 'test_fn_failure': {
+				console.log(
+					`In test_fn_failure, is browser connected: ${ this.global.browser.isConnected() }`
+				);
 				this.failure = { type: 'test', name: event.test.name };
 				this.allure?.failTestStep( event.error );
 				break;
